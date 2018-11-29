6748c039d5723fbc0572c25e83edb94e15c4d0f9
xandhiller <xandhiller@gmail.com>
2018-11-30 09:16:25 +1100

    Pipeline script made with common commands used to facilitate the simulations. Animation of the solar system sourced and avi file made.

---
+++ pipeline.m
@@ -0,0 +1,18 @@
+
+clear all
+close all
+clc
+vredit
+
+% Open editor
+myWorld = vrworld('vrtut3.wrl');
+open(myWorld);
+myCanvas = vr.canvas(myWorld);
+% Simulate
+open('vrtut3.slx')
+% Capture and save as avi
+
+
+
+% Upload to the iphone
+
