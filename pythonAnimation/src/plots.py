aa2a253973950bf15a7d51680af8655a942a6e7b
Alex Hiller <xandhiller@gmail.com>
2019-01-10 12:24:27 +1100

    Simple plot working. Nice black background, small points, cosmic (distant) perspective.

--- pythonAnimation/src/plots.py
+++ pythonAnimation/src/plots.py
@@ -6,14 +6,39 @@
 # 
 ################################################################################
 
+# Libraries
 import pandas as pd
-io = 'pulsarCatalogue.txt'
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+dataSource = './src/pulsarCatalogue.csv'
+
+
+#   plt.show()
 
 
 def main():
-    df = pd.read_csv(io) 
-    print(df)
-    
+    # Ploting setup
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    # Read in data
+    df = pd.read_csv(dataSource) 
+    # Pull position from dataframe
+    x = df['XX']
+    y = df['YY']
+    z = df['ZZ']
+    # Plot data with small points
+    ax.scatter(x,y,z, s=1)
+    # No axes, for prettiness
+    ax.set_axis_off()
+    # Set background to black
+    ax.set_facecolor((0.0, 0.0, 0.0))
+    # Pop up the window with the result
+    plt.show()
+
+# TODO
+# simple animation with spin
+
 
 if __name__ == '__main__':
     main()
---
+++ pythonAnimation/test.c
@@ -0,0 +1,17 @@
+
+/*
+ * Author: Alex Hiller
+ * Year: 2019
+ * Program Description: <++>
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[]) { 
+
+  <++>
+
+  return 0;
+}
