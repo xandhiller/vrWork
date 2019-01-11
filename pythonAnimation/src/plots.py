9cea50df0784397f3691789821b0401258c7025b
Alex Hiller <xandhiller@gmail.com>
2019-01-11 12:17:24 +1100

    Actual clean up. Now working after glitch with set_tight_layout() method.

--- pythonAnimation/src/plots.py
+++ pythonAnimation/src/plots.py
@@ -14,17 +14,21 @@ import os
 from matplotlib.animation import FuncAnimation
 import numpy as np
 
+
 dataSource = './src/pulsarCatalogue.csv'
-
-
-#   plt.show()
 
 
 def main():
     # Ploting setup
     fig = plt.figure()
-    fig.set_tight_layout(True)
+
+#   fig.set_tight_layout(True) # [Causing ERRORS]
+
     ax = fig.add_subplot(111, projection='3d')
+    # No axes, for prettiness
+    ax.set_axis_off()
+    # Set background to black
+    ax.set_facecolor((0.0, 0.0, 0.0))
     # Read in data
     df = pd.read_csv(dataSource) 
     # Pull position from dataframe
@@ -32,39 +36,28 @@ def main():
     y = df['YY']
     z = df['ZZ']
     # Plot data with small points
-
-    ax.scatter(x,y,z, s=1)
-
-    # No axes, for prettiness
-    ax.set_axis_off()
-    # Set background to black
-    ax.set_facecolor((0.0, 0.0, 0.0))
-    # Pop up the window with the result
-
-#       for angle in range(0, 360):
-#           ax.view_init(30, angle)
-#           plt.draw()
-#           plt.pause(.01)
+    my_plot = ax.scatter(x, y, z, s=1, color='C0')
 
     k=1
-    my_plot = ax.scatter(x, y, z, color='C0')
-    def update(k):
-       # Make it spin
-       ax.view_init(30.0, -45.0 + k)
-       # Animation info whilst generating
-       ax.scatter(x,y,z, s=1, color='C0')
-       label = 'timestep {0}'.format(k)
-       print(label)
-       return my_plot, ax
-    anim = FuncAnimation(fig, update, frames=np.arange(0, 60))
-    anim.save('test.gif',
-               dpi=40, 
-               writer='imagemagick')
+    def _update(k):
+      ax.scatter(x,y,z, s=1, color='C0')
+      # Make it spin
+      ax.view_init(30.0, -45.0 + k)
+      # Animation info whilst generating
+      label = 'timestep {0}'.format(k)
+      print(label)
+      return my_plot, ax
+
+    
+    def _animate():
+        anim = FuncAnimation(fig, _update, frames=np.arange(0, 30))
+        anim.save('run.gif',
+                  dpi=300,
+                  writer='imagemagick')
+
+    # What to actually run
+    _animate()
     plt.show()
-
-# TODO
-# simple animation with spin
-
 
 if __name__ == '__main__':
     main()
